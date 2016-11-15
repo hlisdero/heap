7891d820a26ae4c4a1a5bcfa1feda77d3bbfe70f
Fhalfon <facundohalfon@hotmail.com>
2016-11-15 14:54:41 -0300

    Se agregaron mas pruebas

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -70,8 +70,149 @@ static void pruebas_heap_algunos_elementos()
     print_test("el heap fue destruido", true);
 }
 
+static void pruebas_heap_enteros()
+{
+	int  enteros[]={2,8,4,6,9,4};
+	int* valor1 = &enteros[0];
+	int* valor2 = &enteros[1];
+	int* valor3 = &enteros[2];
+	int* valor4 = &enteros[3];
+	int* valor5 = &enteros[4];
+	int* valor6 = &enteros[5];
+	heap_t * heap = heap_crear(intcmp);
+	
+	printf("INICIO DE PRUEBAS CON ENTEROS\n");
+    print_test("crear heap", heap);
+    
+    /* Guardo y borro datos */
+    print_test("encolar 2", heap_encolar(heap, valor1));
+    print_test("encolar 8", heap_encolar(heap, valor2));
+    print_test("la cantidad de elementos es 2", heap_cantidad(heap) == 2);
+    print_test("ver máximo es 8", heap_ver_max(heap) == valor2);
+    print_test("encolar 4", heap_encolar(heap, valor3));
+    print_test("encolar 6", heap_encolar(heap, valor4));
+    print_test("encolar 9", heap_encolar(heap, valor5));
+    print_test("encolar 4", heap_encolar(heap, valor6));
+    print_test("la cantidad de elementos es 6", heap_cantidad(heap) == 6);
+    print_test("ver máximo es 9", heap_ver_max(heap) == valor5);
+    print_test("desencolar dato es 9", heap_desencolar(heap) == valor5);
+    print_test("ver máximo es 8", heap_ver_max(heap) == valor2);
+    print_test("desencolar dato es 8", heap_desencolar(heap) == valor2);
+    print_test("el heap no está vacío", !heap_esta_vacio(heap));
+    print_test("ver máximo es 6", heap_ver_max(heap) == valor4);
+    print_test("desencolar dato es 6", heap_desencolar(heap) == valor4);
+    print_test("desencolar dato es 4", heap_desencolar(heap) == valor6);
+    print_test("desencolar dato es 4", heap_desencolar(heap) == valor3);
+    print_test("desencolar dato es 2", heap_desencolar(heap) == valor1);
+
+    /* Ahora está vacío */
+    print_test("el heap está vacío", heap_esta_vacio(heap));
+
+    /* Destruyo el heap */
+    heap_destruir(heap, NULL);
+    print_test("el heap fue destruido", true);
+}
+    
+static void pruebas_heap_dinamica()
+{
+	int* valor1 = malloc(sizeof(int));
+	int* valor2 = malloc(sizeof(int));
+	int* valor3 = malloc(sizeof(int));
+	int* valor4 = malloc(sizeof(int));
+	
+	*valor1 = 10;
+	*valor2 = 5;
+	*valor3 = 35;
+	*valor4 = 7;
+	
+	heap_t * heap = heap_crear(intcmp);
+	printf("INICIO DE PRUEBAS CON MEMORIA DINAMICA\n");
+    print_test("crear heap", heap);
+	
+	/* Guardo y borro datos */
+	print_test("encolar 10",heap_encolar(heap, valor1));
+	print_test("encolar 5",heap_encolar(heap, valor2));
+	print_test("encolar 35",heap_encolar(heap, valor3));
+	print_test("encolar 7",heap_encolar(heap, valor4));
+	print_test("el heap no está vacío", !heap_esta_vacio(heap));
+	print_test("ver máximo es 35", heap_ver_max(heap) == valor3);
+	
+    
+    /* Destruyo el heap */
+    heap_destruir(heap, free);
+    print_test("el heap fue destruido", true);
+}
+
+static void pruebas_heapsort()
+{
+	char * arreglo[] = {"Juan", "Pepe", "Jose", "Fulano","Mengano"};
+	char * arreglo_ordenado[] = {"Fulano", "Jose", "Juan", "Mengano", "Pepe"};
+	heap_sort((void**)arreglo,5,comparar_strings);
+	printf("Arreglo pos 0: %s\n",arreglo[0]);
+	printf("Arreglo pos 1: %s\n",arreglo[1]);
+	printf("Arreglo pos 2: %s\n",arreglo[2]);
+	printf("Arreglo pos 3: %s\n",arreglo[3]);
+	printf("Arreglo pos 4: %s\n",arreglo[4]);
+	
+	printf("INICIO DE PRUEBAS CON HEAPSORT\n");
+	
+	bool ok = true;
+	int i;
+	for (i = 0; i<5 ; i++)
+	{
+		if (comparar_strings(arreglo[i],arreglo_ordenado[i])!=0)
+		{
+			ok = false;
+			break;
+		}
+	}
+	print_test("Se ordeno el arreglo",ok);
+	print_test("El primero elemento es Fulano",comparar_strings(arreglo[0],arreglo_ordenado[0])==0);
+	print_test("El segundo elemento es Jose",comparar_strings(arreglo[1],arreglo_ordenado[1])==0);
+	print_test("El tercer elemento es Juan",comparar_strings(arreglo[2],arreglo_ordenado[2])==0);
+	print_test("El cuarto elemento es Mengano",comparar_strings(arreglo[3],arreglo_ordenado[3])==0);
+	print_test("El quinto elemento es Pepe",comparar_strings(arreglo[4],arreglo_ordenado[4])==0);
+}
+  
+static void pruebas_heap_muchos_elementos(int volumen)
+{
+	heap_t * heap = heap_crear(intcmp);
+	printf("INICIO DE PRUEBAS MASIVAS\n");
+	print_test("crear heap", heap);
+	int i;
+	bool ok = true;
+	for (i=0; i<volumen ; i++)
+	{
+		ok = heap_encolar(heap,&i);
+		if (!ok) {
+			break;
+		}
+	}
+    print_test("Se encolaron todos los datos",ok);
+    print_test("El heap no esta vacio",!heap_esta_vacio(heap));
+	print_test("la cantidad de elementos es 5000", heap_cantidad(heap) == volumen);
+	print_test("ver máximo es 5000", heap_ver_max(heap) == &i);
+    
+    /* borro los elementos */
+    ok = true;
+    for (i=0 ; i<=volumen ; i++){
+		heap_desencolar(heap);
+	}
+	print_test("Se desencolaron todos los datos",ok);
+	print_test("El heap esta vacio",heap_esta_vacio(heap));
+	print_test("el heap tiene 0 elementos",heap_cantidad(heap) == 0);
+	/* Destruyo el heap */
+    heap_destruir(heap, NULL);
+    print_test("el heap fue destruido", true);
+}
+	
+    
 void pruebas_heap_alumno()
 {
     pruebas_heap_vacio();
     pruebas_heap_algunos_elementos();
+    pruebas_heap_enteros();
+    pruebas_heap_dinamica();
+    pruebas_heapsort();
+    pruebas_heap_muchos_elementos(5000);
 }
